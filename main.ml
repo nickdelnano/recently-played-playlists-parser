f480bb6d1d3a5c63dd50b8d2e9bd468804e117f2
ndelnano <nickdelnano@gmail.com>
2019-01-30 19:38:00 +0000

    Document toks for playlists in main.ml

--- main.ml
+++ main.ml
@@ -21,10 +21,10 @@ let make_playlist (username : string) (playlist_name  : string) (description : s
 (*
 (100 most played from Jan 1 2016 - Jan 1 2017) AND (Tracks played < 2 times between Jan 1 2017 - Jan 1 2018)
 *)
-(* let toks = [Tok_Playlist; Tok_Time_Begin("1451649600"); Tok_Time_End("1483272000"); Tok_Comparator(2); Tok_Count(1); Tok_Limit(100); Tok_Filter_End; Tok_And; Tok_Playlist; Tok_Time_Begin("1483272000"); Tok_Time_End("1514808000"); Tok_Comparator(0); Tok_Count(2); Tok_Filter_End; Tok_End];; *)
+let toks = [Tok_Playlist; Tok_Time_Begin("1451649600"); Tok_Time_End("1483272000"); Tok_Comparator(2); Tok_Count(1); Tok_Limit(100); Tok_Filter_End; Tok_And; Tok_Playlist; Tok_Time_Begin("1483272000"); Tok_Time_End("1514808000"); Tok_Comparator(0); Tok_Count(2); Tok_Filter_End; Tok_End];;
 
 (* Top 100 most played *)
-let toks = [Tok_Playlist; Tok_Comparator(2); Tok_Count(1); Tok_Limit(100); Tok_Filter_End; Tok_End];;
+(* let toks = [Tok_Playlist; Tok_Comparator(2); Tok_Count(1); Tok_Limit(100); Tok_Filter_End; Tok_End];; *)
 
 (* Make `username` match the `username` column in the MySQL `users` table. *)
 let username = "test_user" in
